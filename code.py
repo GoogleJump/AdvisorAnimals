7a6e2ed9835dec47101f283d46a0398b6693a952
Ben Margolin <bmargolin@gmail.com>
2014-03-06 13:09:26 -0800

    a sample loop

--- code.py
+++ code.py
@@ -1,7 +1,15 @@
 
 def helloWorld():
   print "I'm just going to leave this right here."
+
+def listBuilder():
+  b = []
+  for x in range(5):
+    b.append(10 * x)
+  return b
+
 for i in range(5):
   helloWorld()
+print listBuilder()
 
 
